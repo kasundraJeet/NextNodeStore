b4cbc776a0589fc63b61f82fd93092de0481b299
kasundraJeet <kasundrajeet29@gmail.com>
2024-08-18 10:27:37 +0530

    added new checkout from

--- classic90/src/app/check-out/page.js
+++ classic90/src/app/check-out/page.js
@@ -10,7 +10,34 @@ return(
       <Section container="regular">
         <div className="two-col">
       <div>
-    hello
+    <ul className="checkout-tab-list">
+      <li>
+        <button className="active">Shipping</button>
+      </li>
+      <li>
+        <button>Payment</button>
+      </li>
+      </ul>
+      <form className="check-out-form">
+        <div>
+          <h4>Contact</h4>
+          <div className="two-col">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="email" placeholder="Email" />
+          <input type="number" placeholder="Phone No." />
+          </div>
+        </div>
+        <div>
+        <h4>Shipping Address</h4>
+        <div className="two-col">
+          <select>
+            <option selected>India</option>
+          </select>
+        </div>
+        </div>
+  <button className="btn type1">Coutinue To Payment</button>
+      </form>
       </div>
     <div>
         <CheckOutItems />
@@ -19,4 +46,4 @@ return(
 </Section>
     </LayoutWrapper>
     )
-}
+} 
